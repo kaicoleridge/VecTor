ec988c5d5bfcfc63715304040aa6917258688f33
Kai Coleridge <kaicoleridge13@gmail.com>
2022-04-11 09:35:44 +0100

    commit

---
+++ commands/invite.js
@@ -0,0 +1,8 @@
+module.exports ={
+    name: "invite",
+    description: "Server invite link",
+    execute(message, args){
+        message.channel.send("----- Here you go ----- ")
+        message.channel.send("🔗 Join NUM Discord Server > https://discord.gg/B6ERp7ehDD ")
+    }
+}
