d37f7b880e3f76a18345eb6052836fe5a71fffab
Kai Coleridge <kaicoleridge13@gmail.com>
2022-04-11 11:28:17 +0100

    main

--- commands/help.js
+++ commands/help.js
@@ -7,9 +7,17 @@ module.exports = {
         .setImage('attachment://help.png')
         .setColor('#4c31e8')
         .setTitle('Get Help with VecTor')
-        .setDescription("Here is a list of all current commands I can do! Pick a command below and see the magic I can perform!")
+        .setDescription("Here is a list of all current commands I can do! Pick a command below and see the magic I can perform! ✨")
         .addFields(
-           {name: ">ping", value: "Test the bot's latency"},
+           {name: ">ping 🏓", value: "Test the bot's latency, to see if I'm working correcly!"},
+           {name: ">invite 🔗", value: "Get the invite link for the NUM Discord Server"},
+           {name: ">meme 🐸", value: "Get a random meme"},
+           {name: ">stats 📈", value: "Show Discord server stats ie. member count etc."},
+           {name: ">report 🔴" ,value: "Report a user to the NUM Discord Server"},
+           {name: ">twitter 🐦",   value:"Keep up to date with me on twitter!"},
+           {name: ">movieShowings 🍿", value: "Get the current movie showings for the NUM Cinema"},
+           {name: "💜-- MORE COMMANDS COMING SOON!--💜", value: "LOOK OUT FOR MORE UPCOMING COMMANDS!"}
+           
         )
         message.channel.send({ embeds: [helpEmbed], files: ['./imgs/help.png'] });
         }
--- commands/invite.js
+++ commands/invite.js
@@ -2,7 +2,11 @@ module.exports ={
     name: "invite",
     description: "Server invite link",
     execute(message, args){
-        message.channel.send("----- Here you go ----- ")
-        message.channel.send("🔗 Join NUM Discord Server > https://discord.gg/B6ERp7ehDD ")
+        message.channel.send(` **🟣 INVITE LINK 🟣**`)
+        message.channel.send("🔗 Join the NUM Discord Server > https://discord.gg/B6ERp7ehDD ")
+       
+       
     }
+    
+    
 }
