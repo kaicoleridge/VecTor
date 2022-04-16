273054f9b5a88b68f0ebce4b2069e2e181f04943
Kai Coleridge <kaicoleridge13@gmail.com>
2022-04-16 10:54:46 +0100

    commit

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -5,10 +5,15 @@ module.exports = {
     description: "Purges a certain amount of messages",
     execute(message, args) {
         let amountToDelete = args[0];
-        if(!amountToDelete) return message.channel.send("Please specify an amount of messages to delete!\n " + "`" + ">purge <amount>`");
-        if(amountToDelete > 100) return message.channel.send("Please specify an amount of messages less than 100!");
-        if(amountToDelete < 2) return message.channel.send("Please specify an amount of messages greater than 1!");
-        message.channel.bulkDelete(amountToDelete);
+        if(!amountToDelete) return message.reply("Please specify an amount of messages to delete!\n" + "`" + ">purge <amount>`");
+        if(amountToDelete > 100) return message.reply("Please specify an amount of messages less than 100!");
+        if(amountToDelete < 2) return message.reply("Please specify an amount of messages greater than 1!");
+
+        message.channel.bulkDelete(amountToDelete, true).catch(err => {
+            console.log(err);
+            message.channel.send("Error: " + err);
+        });
+
         const purgeEmbed = new MessageEmbed()
         .setTitle("🗑️ Deleted " + amountToDelete + " messages!")
         .setColor('#4c31e8')
@@ -21,7 +26,7 @@ module.exports = {
         .addField('Purged by', message.author.tag)
         message.channel.send({embeds: [purgeEmbed]})
         .then(msg => {
-          setTimeout(() => msg.delete(), 10000)
+          setTimeout(() => msg.delete(), 3000)
         })
         .catch("Error deleting message");
     }
