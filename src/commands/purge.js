09b6d19656539991f04d89d9ffcbced4246a9bed
Kai Coleridge <kaicoleridge13@gmail.com>
2022-04-16 10:07:54 +0100

    commits to purge.js file

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -1,38 +1,31 @@
 const {MessageEmbed} = require('discord.js');
-const {Permissions} = require('discord.js');
 
-module.exports = { 
-    name: "purge",
+module.exports = {
+    name: 'purge',
     description: "Purges a certain amount of messages",
-    Permissions: ['MANAGE_MESSAGES'],
-    execute(message, args, client, member) {
-        
-
-            message.channel.bulkDelete(args[0]).then(() => {
-                const purgeEmbed = new MessageEmbed()
-                .setTitle("Deleted " + args[0] + " messages!")
-                .setColor('#4c31e8')
-                .setThumbnail('')
-                .addFields(
-                    {name: "Purged", value: `${args[0]} messages`, inline: false},
-                    {name: "Purged by", value: `${message.author.username}`, inline: false}
-                );
-                message.channel.send({embeds: [purgeEmbed]});
-            });
-
-           
-
-            if(args[0] > 50){
-                const purgeEmbed = new MessageEmbed()
-                .setTitle("Error")
-                .setColor('#4c31e8')
-                .setThumbnail('')
-                .addFields(
-                    {name: "Error", value: "You can only purge up to 100 messages at a time!", inline: false},
-                    {name: "Purged by", value: `${message.author.username}`, inline: false}
-                );
-                message.channel.send({embeds: [purgeEmbed]});
-            }
-            
+    userPermissions: ['MANAGE_MESSAGES'],
+    execute(message, args) {
+        if(!args[0]) return message.channel.send("Please specify the amount of messages you want to delete! \n **Usage:** `>purge <amount>`");
+        if(isNaN(args[0])) return message.channel.send("Please specify a number!");
+        if(args[0] > 100) return message.channel.send("Please specify a number less than 100!");
+        if(args[0] < 2) return message.channel.send("Please specify a number greater than 1!");
+        message.channel.bulkDelete(args[0])
+        .then(messages => {
+            const purgeEmbed = new MessageEmbed()
+            .setTitle("Deleted " + messages.size + " messages!")
+            .setColor('#4c31e8')
+            .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
+            .addField('Purged by', message.author.tag)
+            .setTimestamp()
+            purgeEmbed.setFooter({
+                text: "Powered by VecTor"
+            })
+            .addField('Purged', `${messages.size} messages`)
+            message.channel.send({embeds: [purgeEmbed]})
+        }
+        )
+        .catch(error => {
+            console.log(error);
+        });
     }
 }
