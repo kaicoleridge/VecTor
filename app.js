03c3f97945078cace3ac889e8f5ceb080c88e85a
Kai Coleridge <kai@coleridge.dev>
2022-12-09 21:08:28 +0000

    cleaned up format

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntentBits, Collection, ActivityType } = require('discord.js');
-const { token } = require('./config.json');
-const { filteredWords } = require('./config.json');
+const {Client, Events, GatewayIntentBits, Collection, ActivityType} = require('discord.js');
+const {token} = require('./config.json');
+const {filteredWords} = require('./config.json');
 
 
 // Create a new client instance
-const client = new Client({ intents: [
-	GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ]
+});
 
 client.commands = new Collection();
 const user = client.users.cache.get('user id');
@@ -24,30 +26,30 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 
 // executes commands in commands directory
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
 }
 
 // executes event files
 for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
 }
 
 client.on('guildMemberAdd', member => {
-   var role = member.guild.roles.cache.find(role => role.name === 'Member');
-   member.roles.add(role);
+    var role = member.guild.roles.cache.find(role => role.name === 'Member');
+    member.roles.add(role);
 });
 
 
@@ -56,86 +58,90 @@ client.on('guildMemberAdd', member => {
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
-	client.user.setActivity({
-		name: '/help',
-		type: ActivityType.Listening,
-	});
+    client.user.setActivity({
+        name: '/help',
+        type: ActivityType.Listening,
+    });
 
 });
 
 
 // detect messages for profanity // offensive words
 client.on('messageCreate', message => {
-	const logChannel = client.channels.cache.get('1045417263546773526');
-	const user = message.author;
-	
-	
-	let detected = false;
-	filteredWords.forEach(word => {
-		if (message.content.toLowerCase().includes(word)) {
-			detected = true;
-			
-		}
-	});
-
-	
-	
-	
-	if (detected) {
-		if(userMap.has(user.id)) {
-			userMap.set(user.id, userMap.get(user.id) + 1);
-		} else {
-			userMap.set(user.id, 1);
-		}
+    const logChannel = client.channels.cache.get('1045417263546773526');
+    const user = message.author;
 
 
-	console.log(userMap.get(user.id));
-		message.delete();		
-		message.channel.send(`${message.author} ⚠️ Your message contained a word that is not allowed in this server. You have been warned.`);
-		message.author.send( `${message.author}` + " \n" + "**👮 AUTO MODERATION**"  + "\n" + "\n" + "⚠️ We detected a word that is not allowed in this server. You will be banned automatically after 3 offences " + "\n\n" + "Word detected: " + "**" +  message.content + "**"  );
-		logChannel.send(`A message was deleted in ${message.channel} by ${message.author} because it contained a filtered word.`);
+    let detected = false;
+    filteredWords.forEach(word => {
+        if (message.content.toLowerCase().includes(word)) {
+            detected = true;
 
-		// ban user if they have 3 warnings
-		if (userMap.get(user.id) === 3) {
-			const reason = message.member.ban({ reason: 'Breaking server rules' });
-			logChannel.send(`${message.author} was **banned** from the server for ${reason}.`);
-		    
-		}
+        }
+    });
 
 
-	}
 
-	
+
+    if (detected) {
+        if (userMap.has(user.id)) {
+            userMap.set(user.id, userMap.get(user.id) + 1);
+        } else {
+            userMap.set(user.id, 1);
+        }
+
+
+        console.log(userMap.get(user.id));
+        message.delete();
+        message.channel.send(`${message.author} ⚠️ Your message contained a word that is not allowed in this server. You have been warned.`);
+        message.author.send(`${message.author}` + " \n" + "**👮 AUTO MODERATION**" + "\n" + "\n" + "⚠️ We detected a word that is not allowed in this server. You will be banned automatically after 3 offences " + "\n\n" + "Word detected: " + "**" + message.content + "**");
+        logChannel.send(`A message was deleted in ${message.channel} by ${message.author} because it contained a filtered word.`);
+
+        // ban user if they have 3 warnings
+        if (userMap.get(user.id) === 3) {
+            const reason = message.member.ban({
+                reason: 'Breaking server rules'
+            });
+            logChannel.send(`${message.author} was **banned** from the server for ${reason}.`);
+
+        }
+
+
+    }
+
+
 });
 
 
 
 // When slash command is executed check if it exists and execute it
 client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
 
-	try {
-		command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+    try {
+        command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        interaction.reply({
+            content: 'There was an error while executing this command!',
+            ephemeral: true
+        });
+    }
 
 
-	
-	console.log(interaction);
+
+    console.log(interaction);
 
 });
 
 
-		
+
 
 // Log in to Discord with your client's token
 client.login(token);
-	
